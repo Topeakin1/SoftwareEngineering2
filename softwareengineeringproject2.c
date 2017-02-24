324e71f54c5125d41c27bd42ccfe41b1a2a0ddbc
Topeakin1 <temitope.akinwale@ucdconnect.ie>
2017-02-24 13:23:44 +0000

    main function

--- softwareengineeringproject2.c
+++ softwareengineeringproject2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "Assignment2Header.h"
+int NumberOfPlayers(); //function prototype
 
+int main(void) {
+	srand(time(NULL));
+	Player players[PLAYER_MAX_SIZE]; 
+	int slots;
+	int playersCount;
+	printf("%d", RandInt(4, 10));
+}
