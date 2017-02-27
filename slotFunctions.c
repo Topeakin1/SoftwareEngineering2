ffb20301d5c61e369b1b082af65081d7a773f542
Topeakin1 <temitope.akinwale@ucdconnect.ie>
2017-02-27 16:37:50 +0000

    functiom
    changed a few things

--- slotFunctions.c
+++ slotFunctions.c
@@ -2,3 +2,41 @@
 #include <time.h>
 #include <stdlib.h>
 #include "Assignment2Header.h"
+
+	typedef struct Slots
+	{
+		char SlotArray[20];
+    };
+int RandInt(int min, int max);
+
+int main(void) 
+{
+	int i;
+	int min;
+	int max;
+	char SlotArray[20];
+
+	{   for(i=0; SlotArray[i]<100; i++) {
+		struct Slot;
+		printf("Please enter in the amount of slots\n");
+		scanf("%c",SlotArray[i]); 
+		
+	}
+}
+{
+    max -= min;
+    int upperBound = RAND_MAX - RAND_MAX % max;
+    int randInt;
+
+    do
+    {
+        randInt = rand();
+    }while(randInt >= upperBound);
+
+    randInt %= max + 1;
+    
+    randInt += min;
+
+    return randInt;
+}
+}
