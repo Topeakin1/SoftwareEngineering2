73062f3484c23c2059891b8c88cc24fda5e19630
Topeakin1 <temitope.akinwale@ucdconnect.ie>
2017-03-01 13:19:09 +0000

    did the slots
    did everything you asked but couldnt figure out how to assign players to
    slots ,how do i do that??

--- slotFunctions.c
+++ slotFunctions.c
@@ -1,52 +1,21 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 #include "Assignment2Header.h"
 #define ASSIGNMENT2HEADER_H_
-#define ASSIGNMENT2HEADER_H_
-
-// Defined constant variables
-#define PLAYER_MAX_SIZE 6
-#define SLOT_MAX_SIZE 6
-#define MAX_STRING_LENGTH 20
-
-	Slot{
-    int slotTypeNum;
-    char slotType[MAX_STRING_LENGTH];
-    int CurrentPlayer;
-} ;
+void SlotFunction(int NumberOfSlots,struct Slot Slots[]) {
+	char SlotTypes[3][15] = {"Level Ground","Hill","City"};
+	int i, random;
+	srand(time(NULL));
 	
-int RandInt(int min, int max);
-
-int main(void) 
-{
-	int i; 
-	int min;
-	int max;
-	char SlotArray[20];
-
-	{   for(i=0; SlotArray[i]<MAX_STRING_LENGTH; i++) {
-		struct Slot;
-		printf("Please enter in the amount of slots\n");
-		scanf("%c",SlotArray[i]); 
-		
+	for(i=0; i<NumberOfSlots; i++) {
+		random=rand() %3;
+		strcpy(Slots[i].slotType,SlotTypes[random]); //assigning my slots
 	}
+	for(i=0; i<NumberOfSlots; i++) {
+		printf("| %s |\n", Slots[i].slotType); //printing out to check
+	}
+	
 }
-{
-    max -= min;
-    int upperBound = RAND_MAX - RAND_MAX % max;
-    int randInt;
 
-    do
-    {
-        randInt = rand();
-    }while(randInt >= upperBound);
-
-    randInt %= max + 1;
-    
-    randInt += min;
-    randInt= SlotArray[i];
-
-    return randInt;
-}
-}
